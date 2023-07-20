3fe4b593b9b30bbafb983d81f8ebc2114efecfd2
6jjdaboss9 <136691330+6jjdaboss9@users.noreply.github.com>
2023-07-20 16:15:45 +0700

    Enhance data encryption

---
+++ zebra_house_cat.rb
@@ -0,0 +1,263 @@
+#!/usr/bin/ruby
+
+# The Road to Resilience
+
+#1
+
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    puts "Hi, I'm #{@name}, and I'm #{@age} years old."
+  end
+end
+
+#2
+
+class Resilience
+  def initialize(challenges, attitude)
+    @challenges = challenges
+    @attitude = attitude
+  end
+
+  def develop
+    puts "Building resilience is all about overcoming #{@challenges} and taking on a #{@attitude} attitude."
+  end
+end
+
+#3
+
+class Strategy
+  def initialize(skills, mindset)
+    @skills = skills
+    @mindset = mindset
+  end
+
+  def implement
+    puts "The key to resilience is developing the #{@skills} and #{@mindset} necessary to take on life's challenges."
+  end
+end
+
+#4
+
+class Courage
+  def initialize(doubt, fear)
+    @doubt = doubt
+    @fear = fear
+  end
+
+  def confront
+    puts "Having the courage to confront our #{@doubt} and #{@fear} is essential for resilience."
+  end
+end
+
+#5
+
+class Compassion
+  def initialize(love, understanding)
+    @love = love
+    @understanding = understanding
+  end
+
+  def nurture
+    puts "Nurturing our #{@love} and #{@understanding} for ourselves is essential for resilience."
+  end
+end
+
+#6
+
+class SelfCare
+  def initialize(exercise, nutrition)
+    @exercise = exercise
+    @nutrition = nutrition
+  end
+
+  def practice
+    puts "Practicing self-care through #{@exercise} and #{@nutrition} is essential for resilience."
+  end
+end
+
+#7
+
+class SelfAwareness
+  def initialize(thoughts, feelings)
+    @thoughts = thoughts
+    @feelings = feelings
+  end
+
+  def recognize
+    puts "Recognizing our #{@thoughts} and #{@feelings} is essential for resilience."
+  end
+end
+
+#8
+
+class ProblemSolving
+  def initialize(conflict, solutions)
+    @conflict = conflict
+    @solutions = solutions
+  end
+
+  def develop
+    puts "Developing problem-solving skills to tackle #{@conflict} and find #{@solutions} is essential for resilience."
+  end
+end
+
+#9
+
+class Flexibility
+  def initialize(adapt, learn)
+    @adapt = adapt
+    @learn = learn
+  end
+
+  def cultivate
+    puts "Cultivating flexibility to #{@adapt} and #{@learn} is essential for resilience."
+  end
+end
+
+#10
+
+class Strength
+  def initialize(toughness, dedication)
+    @toughness = toughness
+    @dedication = dedication
+  end
+
+  def build
+    puts "Building strength through #{@toughness} and #{@dedication} is essential for resilience."
+  end
+end
+
+#11
+
+class Connection
+  def initialize(friends, family)
+    @friends = friends
+    @family = family
+  end
+
+  def cultivate
+    puts "Cultivating connections with #{@friends} and #{@family} is essential for resilience."
+  end
+end
+
+#12
+
+class Gratitude
+  def initialize(appreciate, acknowledge)
+    @appreciate = appreciate
+    @acknowledge = acknowledge
+  end
+
+  def embrace
+    puts "Embracing gratitude to #{@appreciate} and #{@acknowledge} is essential for resilience."
+  end
+end
+
+#13
+
+class Hope
+  def initialize(believe, dream)
+    @believe = believe
+    @dream = dream
+  end
+
+  def cultivate
+    puts "Cultivating hope to #{@believe} and #{@dream} is essential for resilience."
+  end
+end
+
+#14
+
+class SelfReflection
+  def initialize(reflect, analyze)
+    @reflect = reflect
+    @analyze = analyze
+  end
+
+  def practice
+    puts "Practicing self-reflection to #{@reflect} and #{@analyze} is essential for resilience."
+  end
+end
+
+#15
+
+class Acceptance
+  def initialize(reality, change)
+    @reality = reality
+    @change = change
+  end
+
+  def embrace
+    puts "Embracing acceptance of #{@reality} and #{@change} is essential for resilience."
+  end
+end
+
+#16
+
+class Balance
+  def initialize(unwinding, personal)
+    @unwinding = unwinding
+    @personal = personal
+  end
+
+  def maintain
+    puts "Maintaining balance through #{@unwinding} and #{@personal} activities is essential for resilience."
+  end
+end
+
+#17
+
+class Fun
+  def initialize(smile, laugh)
+    @smile = smile
+    @laugh = laugh
+  end
+
+  def enjoy
+    puts "Enjoying life with a #{@smile} and #{@laugh} is essential for resilience."
+  end
+end
+
+#18
+
+class Optimism
+  def initialize(positive, enthusiasm)
+    @positive = positive
+    @enthusiasm = enthusiasm
+  end
+
+  def cultivate
+    puts "Cultivating optimism through #{@positive} thoughts and #{@enthusiasm} is essential for resilience."
+  end
+end
+
+#19
+
+class Patience
+  def initialize(wait, understand)
+    @wait = wait
+    @understand = understand
+  end
+
+  def cultivate
+    puts "Cultivating patience to #{@wait} and #{@understand} is essential for resilience."
+  end
+end
+
+#20
+
+class Growth
+  def initialize(learn, improve)
+    @learn = learn
+    @improve = improve
+  end
+
+  def cultivate
+    puts "Cultivating growth through #{@learn}ing and #{@improve}ing is essential for resilience."
+  end
+end
